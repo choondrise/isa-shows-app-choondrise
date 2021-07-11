bbe01e081afd563281644e20397bd5e3fe9cf28f
Choondrise <loboximax@gmail.com>
2021-07-12 00:28:54 +0200

    Add email and password verification and initLoginButton to LoginActivity

--- Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/LoginActivity.kt
+++ Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/LoginActivity.kt
@@ -1,4 +1,43 @@
 package com.choondrise.shows_hrvoje_brajko
 
-class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.choondrise.shows_hrvoje_brajko.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initLoginButton()
+    }
+
+    private fun initLoginButton() {
+        binding.loginButton.setOnClickListener {
+            val intent = WelcomeActivity.buildIntent(
+                this,
+                binding.editTextEmail.text.toString(),
+                binding.editTextPassword.text.toString())
+
+            if (validateEmail(binding.editTextEmail.text.toString()) &&
+                validatePassword(binding.editTextPassword.text.toString()))
+                startActivity(intent)
+        }
+    }
+
+    private fun validateEmail(email: String) : Boolean {
+        if (email.isEmpty()) return false
+
+        val regex = "^[A-Za-z](.*)([@])(.+)(\\.)(.{1,})".toRegex()
+        return email.matches(regex)
+    }
+
+    private fun validatePassword(password: String) : Boolean {
+        if (password.isEmpty()) return false
+        return password.length > 5
+    }
 }
