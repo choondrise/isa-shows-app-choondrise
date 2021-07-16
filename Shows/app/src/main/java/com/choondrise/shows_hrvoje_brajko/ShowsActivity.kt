4b6378cb66c1e42446d82481bf8e401736bdc076
Choondrise <loboximax@gmail.com>
2021-07-16 12:14:37 +0200

    Create shows_activity and add it to manifest

--- Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/ShowsActivity.kt
+++ Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/ShowsActivity.kt
@@ -3,9 +3,9 @@ package com.choondrise.shows_hrvoje_brajko
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ShowsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shows)
     }
 }
