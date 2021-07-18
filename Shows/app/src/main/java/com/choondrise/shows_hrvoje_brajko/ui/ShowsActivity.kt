2df01f7afdc0ad9b200f8d3a1a6cd9aeb48450c7
Choondrise <loboximax@gmail.com>
2021-07-18 12:14:27 +0200

    Add initRecycleView and list of shows to ShowsActivity

--- Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/ui/ShowsActivity.kt
+++ Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/ui/ShowsActivity.kt
@@ -4,7 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.choondrise.shows_hrvoje_brajko.databinding.ActivityShowsBinding
+import com.choondrise.shows_hrvoje_brajko.model.Show
 
 class ShowsActivity : AppCompatActivity() {
 
@@ -22,6 +25,25 @@ class ShowsActivity : AppCompatActivity() {
         }
     }
 
+    private val shows = listOf(
+        Show("Krim Tim 2",
+            "The three police inspectors are solving cases around the city of Zagreb.",
+            R.drawable.krim_tim_2),
+        Show("Zabranjena ljubav",
+            "Brother and sister twins separated at birth meet eachother nearly " +
+                "two decades later not knowing about their blood relation. The two fall in love and that is only " +
+                "the peak of the iceberg for what is yet to come.",
+            R.drawable.zabranjena_ljubav),
+        Show("Vecernja skola",
+            "Humorous tv show about a teacher and his middle-aged students " +
+                "who attend night school.",
+            R.drawable.vecernja_skola),
+        Show("Nasa mala klinika",
+            "Sitcom about the staff, patients, guests and all kinds of different " +
+                "events in a small clinic at the end of a town.",
+            R.drawable.nasa_mala_klinika)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShowsBinding.inflate(layoutInflater)
@@ -30,5 +52,11 @@ class ShowsActivity : AppCompatActivity() {
         // val password = intent.extras?.getString(EXTRA_PASSWORD)
 
         setContentView(binding.root)
+
+        initRecycleView()
+    }
+
+    private fun initRecycleView() {
+        binding.showRecycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 }
