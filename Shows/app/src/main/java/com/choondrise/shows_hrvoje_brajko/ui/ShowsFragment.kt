414476207ff23fcfd0abe366eaa64cc08142bf76
Choondrise <loboximax@gmail.com>
2021-07-24 13:57:57 +0200

    Add option to change profile photo in shows fragment

--- Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/ui/ShowsFragment.kt
+++ Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/ui/ShowsFragment.kt
@@ -1,13 +1,20 @@
 package com.choondrise.shows_hrvoje_brajko.ui
 
 import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -18,11 +25,11 @@ import com.choondrise.shows_hrvoje_brajko.R
 import com.choondrise.shows_hrvoje_brajko.databinding.DialogAddReviewBinding
 import com.choondrise.shows_hrvoje_brajko.databinding.DialogProfileDetailsBinding
 import com.choondrise.shows_hrvoje_brajko.databinding.FragmentShowsBinding
-import com.choondrise.shows_hrvoje_brajko.model.Review
-import com.choondrise.shows_hrvoje_brajko.model.Show
-import com.choondrise.shows_hrvoje_brajko.model.ShowsViewModel
-import com.choondrise.shows_hrvoje_brajko.model.preparePermissionsContract
+import com.choondrise.shows_hrvoje_brajko.model.*
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import androidx.core.app.ActivityCompat.*
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 class ShowsFragment : Fragment() {
 
@@ -34,10 +41,15 @@ class ShowsFragment : Fragment() {
 
     private val args: ShowsFragmentArgs by navArgs()
     private val viewModel: ShowsViewModel by viewModels()
+    private lateinit var profilePhotoUri: Uri
 
     private val cameraPermissionForTakingProfilePicture = preparePermissionsContract(onPermissionsGranted = {
-        Toast.makeText(activity, "dao si mi permission a sad samo trebam otvoriti kameru", Toast.LENGTH_SHORT).show()
+        startCameraContractUsingUri()
     })
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
 
     override fun onCreateView(
 
@@ -82,7 +94,6 @@ class ShowsFragment : Fragment() {
         }
         binding.showRecycler.adapter = adapter
     }
-
 
     private fun initShowHideButton() {
         binding.fab.setOnClickListener {
@@ -165,6 +176,46 @@ class ShowsFragment : Fragment() {
                 activity?.finish()
             }
         })
+    }
+
+    private fun startCameraContractUsingUri() {
+        val profilePhotoFile = activity?.let { FileUtil.createImageFile(it) }
+        profilePhotoUri = FileProvider.getUriForFile(requireActivity(), "com.choondrise.shows_hrvoje_brajko" + ".fileprovider", profilePhotoFile!!)
+        // Activity.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        dispatchTakePictureIntent(profilePhotoUri)
+    }
+
+    private fun dispatchTakePictureIntent(profilePhotoUri: Uri) {
+        val takePictureIntent = ActivityResultContracts.TakePicture().createIntent(requireActivity(), profilePhotoUri)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(activity, "An error occurred while opening camera app", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            displayProfilePhoto()
+        }
+    }
+
+    private fun displayProfilePhoto() {
+        Toast.makeText(activity, profilePhotoUri.toString(), Toast.LENGTH_LONG).show()
+        val dialogBinding = DialogProfileDetailsBinding.inflate(layoutInflater)
+        Glide.with(this)
+            .load(profilePhotoUri)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(true)
+            .into(dialogBinding.profileIcon)
+
+        Glide.with(this)
+            .load(profilePhotoUri)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(true)
+            .circleCrop()
+            .into(binding.logoutButton)
     }
 
     override fun onDestroyView() {
