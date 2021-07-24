2f88533a6d9d823d75664f2175bea89c47e4a13e
Choondrise <loboximax@gmail.com>
2021-07-24 20:41:53 +0200

    Create RegisterFragment, fragment_register.xml and RegisterViewModel

---
+++ Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/model/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.choondrise.shows_hrvoje_brajko.model
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+
+}
---
+++ Shows/app/src/main/java/com/choondrise/shows_hrvoje_brajko/ui/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package com.choondrise.shows_hrvoje_brajko.ui
+
+import androidx.fragment.app.Fragment
+
+class RegisterFragment : Fragment() {
+
+}
